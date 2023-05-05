bd5428f7a5817dea08dd27303c903b560d89b210
Alex <ale-dev@outlook.fr>
2023-05-05 11:29:09 +0200

    C'était l'idée de Paul de mettre ça sur le git

---
+++ dist/js/about.0cdee609.js
@@ -0,0 +1,2 @@
+"use strict";(self["webpackChunksensennet_gui"]=self["webpackChunksensennet_gui"]||[]).push([[443],{4335:function(n,e,s){s.r(e),s.d(e,{default:function(){return l}});var u=s(3396);const t={class:"about"},a=(0,u._)("h1",null,"This is an about page",-1),r=[a];function i(n,e){return(0,u.wg)(),(0,u.iD)("div",t,r)}var c=s(89);const o={},f=(0,c.Z)(o,[["render",i]]);var l=f}}]);
+//# sourceMappingURL=about.0cdee609.js.map
