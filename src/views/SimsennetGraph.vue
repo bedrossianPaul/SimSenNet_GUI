9527cd753a17a79051db42eed6322cc0d45759d9
Alex <ale-dev@outlook.fr>
2023-05-05 10:41:38 +0200

    Ajout du Graph

--- src/views/SimsennetGraph.vue
+++ src/views/SimsennetGraph.vue
@@ -1,34 +1,97 @@
 <script>
 import { VNetworkGraph } from "v-network-graph"
 import "v-network-graph/lib/style.css"
-var nodes = {
-  node1: { name: "Node 1" },
-  node2: { name: "Node 2" },
-  node3: { name: "Node 3" },
-  node4: { name: "Node 4" },
-}
+import {computed, reactive, ref, watch} from "vue"
+import * as vNG from "v-network-graph"
+import {ForceLayout,ForceNodeDatum,ForceEdgeDatum} from "v-network-graph/lib/force-layout"
+const zoomLevel = ref(1.5)
 
-var edges = {
-  edge1: { source: "node1", target: "node2" },
-  edge2: { source: "node2", target: "node3" },
-  edge3: { source: "node3", target: "node4" },
-}
+const configs = reactive(
+    vNG.defineConfigs({
+      view: {
+        layoutHandler: new ForceLayout({positionFixedByDrag: false, positionFixedByClickWithAltKey: true}),
+        scalingObjects: true,
+        minZoomLevel: 0.1,
+        maxZoomLevel: 16,
+      },
+    })
+)
+
 export default {
   name: 'SensennetGraph',
   components: {
     VNetworkGraph
   },
+  props:{
+    graph: String
+  },
+  mounted () {
+    this.get_node();
+    this.timer = setInterval(this.get_node, 30000);
+  },
+  beforeUnmount() {
+    clearInterval(this.timer);
+  },
   data(){
     return {
-      nodes: nodes,
-      edges: edges
+      data: reactive({}),
+      config: configs,
+      zoomLevel: zoomLevel,
+      timer: '',
+      d3ForceEnabled: computed({
+        get: () => configs.view.layoutHandler instanceof ForceLayout,
+        set: (value) => {
+          if (value) {
+            configs.view.layoutHandler = new ForceLayout()
+          } else {
+            configs.view.layoutHandler = new vNG.SimpleLayout()
+          }
+        },
+      })
+    }
+  },
+  methods: {
+    get_node() {
+      let request = new XMLHttpRequest();
+      request.open("GET", this.graph, true);
+      request.onload = function () {
+        var jsonResult = JSON.parse(request.responseText);
+        this.data["nodes"] = reactive(jsonResult["nodes"])
+        this.data["edges"] = reactive(jsonResult["edges"])
+        this.data["layouts"] = reactive(jsonResult["layouts"])
+      };
+      request.send();
+      /*this.data["nodes"] = {
+        node1 : { name: "Node 1"},
+        node2 : { name: "Node 2"},
+        node3 : { name: "Node 3"},
+        node4 : { name: "Node 4"}
+      }
+      this.data["edges"] = {
+        edge1: { source: "node1", target: "node2"},
+        edge2: { source: "node2", target: "node3" },
+        edge3: { source: "node3", target: "node4" }
+      }
+      this.data["layouts"] = {
+        nodes: {
+          node1: {
+            x: 0,
+            y: 0,
+            fixed: true, // Unaffected by force
+          },
+        }
+      }*/
     }
   }
 }
 </script>
 
 <template>
- <v-network-graph class="graph" :nodes="nodes" :edges="edges"></v-network-graph>
+  <div class="demo-control-panel">
+    <label for="scaleObj">Scaling Objects:</label><input type="checkbox" v-model="config.view.scalingObjects" name="scaleObj">
+    <label for="forceEnabled">Automatic Node Positioning:</label><input type="checkbox" v-model="d3ForceEnabled" name="forceEnabled">
+  </div>
+ <v-network-graph class="graph" :nodes="data['nodes']" :edges="data['edges']" :layouts="data['layouts']" v-model:zoom-level="zoomLevel" :configs="config"></v-network-graph>
 </template>
 
 <style scoped>
